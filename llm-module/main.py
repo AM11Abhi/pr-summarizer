5494649ef6ec48cade3dc00e44d4e963914e8659
Abhinav <abhinav7268@gmail.com>
2026-03-11 10:22:52 +0530

    Changing model as old not supporting

--- llm-module/main.py
+++ llm-module/main.py
@@ -26,7 +26,7 @@ def main():
 
         genai.configure(api_key=api_key)
 
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
         prompt = f"""
 You are an AI code reviewer.
